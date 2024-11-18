fdd0bca3c87016b21d3e4fa0a87e925af409c673
Mark Taratynov <mark.taratynov@gmail.com>
2024-11-18 09:35:04 +0100

    loading svg

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,12 @@
 export default function Loading() {
-  const loading =
-    "https://www.freeiconspng.com/thumbs/pokeball-png/file-pokeball-png-0.png";
   return (
     <div className="flex justify-center gap-1 items-center my-5">
-      <img className="w-10 animate-bounce" src={loading} alt="" />
-      <p>Loading...</p>
+      <img
+        className="w-10 animate-spin mx-2"
+        src="src/assets/loading.svg"
+        alt=""
+      />
+      <p className="text-2xl">Loading...</p>
     </div>
   );
 }
